b45ffce21952c75166cc40c1e9221f3a8a24860f
Md. Ikramul Hossain <ikramul.hackerslash@gmail.com>
2025-08-03 06:12:40 +0600

    Integrate progress task list api

--- lib/ui/screens/progress_task_list_screen.dart
+++ lib/ui/screens/progress_task_list_screen.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:task_manager_app/ui/widgets/task_card.dart';
+import '../../data/models/task_model.dart';
+import '../../data/service/network_caller.dart';
+import '../../data/urls.dart';
+import '../widgets/centered_circular_progress_indicator.dart';
+import '../widgets/snack_bar_message.dart';
 
 class ProgressTaskListScreen extends StatefulWidget {
   const ProgressTaskListScreen({super.key});
@@ -9,16 +14,59 @@ class ProgressTaskListScreen extends StatefulWidget {
 }
 
 class _ProgressTaskListScreenState extends State<ProgressTaskListScreen> {
+  bool _getProgressTasksInProgress = false;
+  List<TaskModel> _progressTaskList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _getProgressTaskList();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 8),
+        child: Visibility(
+          visible: _getProgressTasksInProgress == false,
+          replacement: CenteredCircularProgressIndicator(),
       child: ListView.builder(
-        itemCount: 10,
+        itemCount: _progressTaskList.length,
         itemBuilder: (context, index) {
-          return TaskCard(taskType: TaskType.progress);
+          return TaskCard(
+            taskType: TaskType.progress,
+            taskModel: _progressTaskList[index],
+            onStatusUpdate: () {
+              _getProgressTaskList();
+            },
+          );
         },
-      ),
+    ),
+        ),
     );
   }
+
+  Future<void> _getProgressTaskList() async {
+    _getProgressTasksInProgress = true;
+    setState(() {});
+
+    NetworkResponse response = await NetworkCaller
+        .getRequest(url: Urls.getProgressTasksUrl);
+
+    if (response.isSuccess) {
+      List<TaskModel> list = [];
+      for (Map<String, dynamic> jsonData in response.body!['data']) {
+        list.add(TaskModel.fromJson(jsonData));
+      }
+      _progressTaskList = list;
+    } else {
+      showSnackBarMessage(context, response.errorMessage!);
+    }
+
+    _getProgressTasksInProgress = false;
+    setState(() {});
+  }
+
 }
